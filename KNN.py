1c87f02e00d176c2007f37f68400d93e3498fd8f
LauraLerebours <lerebourslaura24@gmail.com>
2023-02-22 12:35:58 -0500

    Preliminary feature extraction

---
+++ KNN.py
@@ -0,0 +1,30 @@
+#Code inspired by
+# https://www.kaggle.com/code/papeloto/urban-sound-feature-extraction-knn?scriptVersionId=12264904&cellId=2
+
+import os
+
+import numpy as np
+import pandas as pd
+
+import librosa
+import librosa.display
+import soundfile as sf # librosa fails when reading files on Kaggle.
+
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
+
+audio_path = '\lereb\Downloads\WAV Audio Files\Becca-kick.wav'
+ipd.Audio(audio_path)
+
+x, sr = librosa.load(audio_path,sr=22)
+
+plt.figure(figsize=(12, 5))
+librosa.display.waveplot(x, sr=sr)
+plt.show()
--- Practice.py
+++ Practice.py
@@ -21,7 +21,7 @@ prediction = knn.predict(new_point)
 
 plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
 plt.text(x=new_x-1.7, y=new_y-0.7, s=f"new point, class: {prediction[0]}")
-plt.show()
+#plt.show()
 
 
 # @inproceedings{pons2018atscale,
